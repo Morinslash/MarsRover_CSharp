f530ef4c94b39ea345052fa7eab276c8e2e5bd63
Morinslash <sebastian.elf.kulinski@gmail.com>
2022-09-13 16:23:19 +0200

    Extracting compass class

--- MarsRoverBasic/src/MarsRover.cs
+++ MarsRoverBasic/src/MarsRover.cs
@@ -1,35 +1,18 @@
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace MarsRoverBasic;
 
-public class MarsRover
+public class Compass
 {
-    public MarsRover()
+    public Compass(string direction)
     {
-        Direction = "N";
+        Direction = direction;
     }
 
     public string Direction { get; private set; }
 
-    public string Execute(string command)
-    {
-        foreach (var symbol in command)
-        {
-            if (symbol.Equals('R'))
-            {
-                TurnRight();
-            }
-
-            if (symbol.Equals('L'))
-            {
-                TurnLeft();
-            }
-        }
-
-        return $"0:0:{Direction}";
-    }
-
-    private void TurnRight()
+    public void TurnRight()
     {
         Direction = Direction switch
         {
@@ -40,7 +23,7 @@ public class MarsRover
         };
     }
 
-    private void TurnLeft()
+    public void TurnLeft()
     {
         Direction = Direction switch
         {
@@ -49,5 +32,33 @@ public class MarsRover
             "S" => "E",
             _ => "N"
         };
+    }
+}
+
+public class MarsRover
+{
+    private readonly Compass _compass;
+
+    public MarsRover()
+    {
+        _compass = new Compass("N");
+    }
+
+    public string Execute(string command)
+    {
+        foreach (var symbol in command)
+        {
+            if (symbol.Equals('L'))
+            {
+                _compass.TurnLeft();
+            }
+
+            if (symbol.Equals('R'))
+            {
+                _compass.TurnRight();
+            }
+        }
+
+        return $"0:0:{_compass.Direction}";
     }
 }
