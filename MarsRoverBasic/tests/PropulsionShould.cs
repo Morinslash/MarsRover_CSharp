71f697fdd86bc6769e1dcd54519b735bf6c00431
Morinslash <sebastian.elf.kulinski@gmail.com>
2022-09-16 16:14:09 +0200

    refactor propulsion tests

--- MarsRoverBasic/tests/PropulsionShould.cs
+++ MarsRoverBasic/tests/PropulsionShould.cs
@@ -5,19 +5,21 @@ namespace MarsRoverBasic.tests;
 
 public class PropulsionShould
 {
+    private readonly Propulsion _propulsion;
+
+    public PropulsionShould()
+    {
+        _propulsion = new Propulsion();
+    }
+
     [Fact]
     public void Return_New_Coordinates_That_Results_From_Adding_Move_Vector_Up()
     {
         var expected = new Coordinates { Longitude = 5, Latitude = 6 };
         var origin = new Coordinates { Longitude = 5, Latitude = 5 };
         var moveVector = new Coordinates { Longitude = 0, Latitude = 1 };
-        var grid = new Grid(10);
-
-        var propulsion = new Propulsion();
-
-        var newCoordinates = propulsion.Move(origin, moveVector);
-
-        Assert.Equal(expected, newCoordinates);
+        
+        Assert.Equal(expected, _propulsion.Move(origin, moveVector));
     }
 
     [Fact]
@@ -26,13 +28,8 @@ public class PropulsionShould
         var expected = new Coordinates { Longitude = 5, Latitude = 4 };
         var origin = new Coordinates { Longitude = 5, Latitude = 5 };
         var moveVector = new Coordinates { Longitude = 0, Latitude = -1 };
-        var grid = new Grid(10);
 
-        var propulsion = new Propulsion();
-
-        var newCoordinates = propulsion.Move(origin, moveVector);
-
-        Assert.Equal(expected, newCoordinates);
+        Assert.Equal(expected, _propulsion.Move(origin, moveVector));
     }
 
     [Fact]
@@ -41,13 +38,8 @@ public class PropulsionShould
         var expected = new Coordinates { Longitude = 6, Latitude = 5 };
         var origin = new Coordinates { Longitude = 5, Latitude = 5 };
         var moveVector = new Coordinates { Longitude = 1, Latitude = 0 };
-        var grid = new Grid(10);
-
-        var propulsion = new Propulsion();
-
-        var newCoordinates = propulsion.Move(origin, moveVector);
-
-        Assert.Equal(expected, newCoordinates);
+        
+        Assert.Equal(expected, _propulsion.Move(origin, moveVector));
     }
 
     [Fact]
@@ -56,12 +48,7 @@ public class PropulsionShould
         var expected = new Coordinates { Longitude = 4, Latitude = 5 };
         var origin = new Coordinates { Longitude = 5, Latitude = 5 };
         var moveVector = new Coordinates { Longitude = -1, Latitude = 0 };
-        var grid = new Grid(10);
 
-        var propulsion = new Propulsion();
-
-        var newCoordinates = propulsion.Move(origin, moveVector);
-
-        Assert.Equal(expected, newCoordinates);
+        Assert.Equal(expected, _propulsion.Move(origin, moveVector));
     }
 }
