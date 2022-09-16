56986b85cf80fa240391fd6ab4ac39a36a4f52c2
Morinslash <sebastian.elf.kulinski@gmail.com>
2022-09-16 09:44:30 +0200

    Add overload to move for enabling refactor

--- MarsRoverBasic/src/Propulsion.cs
+++ MarsRoverBasic/src/Propulsion.cs
@@ -5,10 +5,15 @@ public class Propulsion
     public Coordinates Move(Coordinates coordinates, Compass compass, Grid grid) =>
         compass.Direction switch
         {
-            "E" => coordinates with { X = grid.Wrap(coordinates.X + 1) },
             "N" => coordinates with { Y = grid.Wrap(coordinates.Y + 1) },
             "S" => coordinates with { Y = grid.Wrap(coordinates.Y - 1) },
+            "E" => coordinates with { X = grid.Wrap(coordinates.X + 1) },
             "W" => coordinates with { X = grid.Wrap(coordinates.X - 1) },
             _ => coordinates
         };
+
+    public Coordinates Move(Coordinates coordinates, Coordinates moveVector, Grid grid)
+    {
+        return new Coordinates { X = coordinates.X + moveVector.X, Y = coordinates.Y + moveVector.Y };
+    }
 }
--- MarsRoverBasic/tests/PropulsionShould.cs
+++ MarsRoverBasic/tests/PropulsionShould.cs
@@ -63,4 +63,63 @@ public class PropulsionShould
         
         Assert.Equal(expectedPosition, newPosition);
     }
+
+    [Fact]
+    public void Return_New_Coordinates_That_Results_From_Adding_Move_Vector_Up()
+    {
+        var expected = new Coordinates { X = 5, Y = 6 };
+        var origin = new Coordinates{X = 5, Y = 5};
+        var moveVector = new Coordinates{X = 0, Y = 1};
+        var grid = new Grid(10);
+
+        var propulsion = new Propulsion();
+
+        var newCoordinates = propulsion.Move(origin,moveVector,grid);
+        
+        Assert.Equal(expected, newCoordinates);
+    }
+    
+    [Fact]
+    public void Return_New_Coordinates_That_Results_From_Adding_Move_Vector_Down()
+    {
+        var expected = new Coordinates { X = 5, Y = 4 };
+        var origin = new Coordinates{X = 5, Y = 5};
+        var moveVector = new Coordinates{X = 0, Y = -1};
+        var grid = new Grid(10);
+
+        var propulsion = new Propulsion();
+
+        var newCoordinates = propulsion.Move(origin,moveVector,grid);
+        
+        Assert.Equal(expected, newCoordinates);
+    }
+    
+    [Fact]
+    public void Return_New_Coordinates_That_Results_From_Adding_Move_Vector_Right()
+    {
+        var expected = new Coordinates { X = 6, Y = 5 };
+        var origin = new Coordinates{X = 5, Y = 5};
+        var moveVector = new Coordinates{X = 1, Y = 0};
+        var grid = new Grid(10);
+
+        var propulsion = new Propulsion();
+
+        var newCoordinates = propulsion.Move(origin,moveVector,grid);
+        
+        Assert.Equal(expected, newCoordinates);
+    }
+    [Fact]
+    public void Return_New_Coordinates_That_Results_From_Adding_Move_Vector_Left()
+    {
+        var expected = new Coordinates { X = 4, Y = 5 };
+        var origin = new Coordinates{X = 5, Y = 5};
+        var moveVector = new Coordinates{X = -1, Y = 0};
+        var grid = new Grid(10);
+
+        var propulsion = new Propulsion();
+
+        var newCoordinates = propulsion.Move(origin,moveVector,grid);
+        
+        Assert.Equal(expected, newCoordinates);
+    }
 }
